c55ab0ffe10040eb6208e8466f71c0b954307d52
Rishu Chowdhary <hi.10rishu@gmail.com>
2022-08-29 18:53:41 +0530

    Update JDBCCredential.java

--- Concreate/JDBCCredentials.java
+++ Concreate/JDBCCredentials.java
@@ -5,9 +5,9 @@ public class JDBCCredentials {
     public static JDBCCredentials instance = new JDBCCredentials();
 
     private static final String connector = "com.mysql.cj.jdbc.Driver";
-    private static final String url = "jdbc:mysql://localhost:3306/rishu";
+    private static final String url = "jdbc:mysql://localhost:portNumber/database";
     private static final String userName = "root";
-    private static final String passWord = "root@1234";
+    private static final String passWord = "root";
 
     private JDBCCredentials() {
         // No one initialise this constructor Singleton Pattern
